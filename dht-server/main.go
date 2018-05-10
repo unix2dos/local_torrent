dcd023d29b803ee30ee67739ea8e04afcb4316e4
liuwei <levonfly@gmail.com>
2018-05-10 22:54:21 +0800

    firset commit

---
+++ dht-server/main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	_ "github.com/anacrolix/envpprof"
+	"github.com/anacrolix/tagflag"
+
+	"github.com/anacrolix/dht"
+	"github.com/anacrolix/torrent"
+)
+
+var (
+	flags = struct {
+		TableFile string `help:"name of file for storing node info"`
+		Addr      string `help:"local UDP address"`
+	}{
+		TableFile: "/dev/null",
+		Addr:      ":6881",
+	}
+	s *dht.Server
+)
+
+func loadTable() (err error) {
+	added, err := s.AddNodesFromFile(flags.TableFile)
+	log.Printf("loaded %d nodes from table file", added)
+	return
+}
+
+func saveTable() error {
+	return dht.WriteNodesToFile(s.Nodes(), flags.TableFile)
+}
+
+func GlobalBootstrapAddrs() (addrs []dht.Addr, err error) {
+	for _, s := range []string{
+		"127.0.0.1:6881",
+		"172.24.120.28:6881",
+	} {
+		host, port, err := net.SplitHostPort(s)
+		if err != nil {
+			panic(err)
+		}
+		hostAddrs, err := net.LookupHost(host)
+		if err != nil {
+			log.Printf("error looking up %q: %v", s, err)
+			continue
+		}
+		for _, a := range hostAddrs {
+			ua, err := net.ResolveUDPAddr("udp", net.JoinHostPort(a, port))
+			if err != nil {
+				log.Printf("error resolving %q: %v", a, err)
+				continue
+			}
+			addrs = append(addrs, dht.NewAddr(ua))
+		}
+	}
+	if len(addrs) == 0 {
+		err = errors.New("nothing resolved")
+	}
+	return
+}
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	tagflag.Parse(&flags)
+
+	conn, err := torrent.NewUtpSocket("udp", flags.Addr)
+	//conn, err := net.ListenPacket("udp", flags.Addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	s, err = dht.NewServer(&dht.ServerConfig{
+		Conn:          conn,
+		StartingNodes: GlobalBootstrapAddrs,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		s.WriteStatus(w)
+	})
+	err = loadTable()
+	if err != nil {
+		log.Fatalf("error loading table: %s", err)
+	}
+	log.Printf("dht server on %s, ID is %x", s.Addr(), s.ID())
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		ch := make(chan os.Signal)
+		signal.Notify(ch)
+		<-ch
+		cancel()
+	}()
+	//go func() {
+	//	if tried, err := s.Bootstrap(); err != nil {
+	//		log.Printf("error bootstrapping: %s", err)
+	//	} else {
+	//		log.Printf("finished bootstrapping: crawled %d addrs", tried)
+	//	}
+	//}()
+	go func() {
+		for {
+			if tried, err := s.Bootstrap(); err != nil {
+				log.Printf("error bootstrapping: %s", err)
+			} else {
+				log.Printf("finished bootstrapping: crawled %d addrs", tried)
+			}
+			time.Sleep(time.Second * 10) //此处是不停的让别人回复自己, 让新加入的节点变成好节点
+		}
+	}()
+
+	<-ctx.Done()
+	s.Close()
+
+	if flags.TableFile != "" {
+		if err := saveTable(); err != nil {
+			log.Printf("error saving node table: %s", err)
+		}
+	}
+}
---
+++ torrent/main.go
@@ -0,0 +1,197 @@
+// Downloads torrents from the command-line.
+package main
+
+import (
+	"errors"
+	"expvar"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/anacrolix/envpprof"
+	"github.com/anacrolix/tagflag"
+	"github.com/dustin/go-humanize"
+	"github.com/gosuri/uiprogress"
+
+	"github.com/anacrolix/dht"
+	"github.com/anacrolix/torrent"
+	"github.com/anacrolix/torrent/metainfo"
+	"golang.org/x/time/rate"
+)
+
+var progress = uiprogress.New()
+
+func torrentBar(t *torrent.Torrent) {
+	bar := progress.AddBar(1)
+	bar.AppendCompleted()
+	bar.AppendFunc(func(*uiprogress.Bar) (ret string) {
+		select {
+		case <-t.GotInfo():
+		default:
+			return "getting info"
+		}
+		if t.Seeding() {
+			return "seeding"
+		} else if t.BytesCompleted() == t.Info().TotalLength() {
+			return "completed"
+		} else {
+			return fmt.Sprintf("downloading (%s/%s)", humanize.Bytes(uint64(t.BytesCompleted())), humanize.Bytes(uint64(t.Info().TotalLength())))
+		}
+	})
+	bar.PrependFunc(func(*uiprogress.Bar) string {
+		return t.Name()
+	})
+	go func() {
+		<-t.GotInfo()
+		tl := int(t.Info().TotalLength())
+		if tl == 0 {
+			bar.Set(1)
+			return
+		}
+		bar.Total = tl
+		for {
+			bc := t.BytesCompleted()
+			bar.Set(int(bc))
+			time.Sleep(time.Second)
+		}
+	}()
+}
+
+func addTorrents(client *torrent.Client) {
+	for _, arg := range flags.Torrent {
+		t := func() *torrent.Torrent {
+			if strings.HasPrefix(arg, "magnet:") {
+				t, err := client.AddMagnet(arg)
+				if err != nil {
+					log.Fatalf("error adding magnet: %s", err)
+				}
+				go func() {
+					<-t.GotInfo()
+					//t.Info().AddPath("22")
+				}()
+
+				return t
+			} else if strings.HasPrefix(arg, "infohash:") {
+				t, _ := client.AddTorrentInfoHash(metainfo.NewHashFromHex(strings.TrimPrefix(arg, "infohash:")))
+				return t
+			} else {
+				metaInfo, err := metainfo.LoadFromFile(arg)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "error loading torrent file %q: %s\n", arg, err)
+					os.Exit(1)
+				}
+				t, err := client.AddTorrent(metaInfo)
+				if err != nil {
+					log.Fatal(err)
+				}
+				//t.Info().AddPath("/Users/liuwei/golang/src")
+				return t
+			}
+		}()
+		torrentBar(t)
+		go func() {
+			<-t.GotInfo()
+			t.DownloadAll()
+		}()
+	}
+}
+
+var flags = struct {
+	Seed         bool          `help:"seed after download is complete"`
+	Addr         *net.TCPAddr  `help:"network listen addr"`
+	UploadRate   tagflag.Bytes `help:"max piece bytes to send per second"`
+	DownloadRate tagflag.Bytes `help:"max bytes per second down from peers"`
+	Debug        bool
+	tagflag.StartPos
+	Torrent []string `arity:"+" help:"torrent file path or magnet uri"`
+}{
+	UploadRate:   -1,
+	DownloadRate: -1,
+}
+
+func stdoutAndStderrAreSameFile() bool {
+	fi1, _ := os.Stdout.Stat()
+	fi2, _ := os.Stderr.Stat()
+	return os.SameFile(fi1, fi2)
+}
+
+func GlobalBootstrapAddrs() (addrs []dht.Addr, err error) {
+	for _, s := range []string{
+		"172.24.120.28:6881",
+	} {
+		host, port, err := net.SplitHostPort(s)
+		if err != nil {
+			panic(err)
+		}
+		hostAddrs, err := net.LookupHost(host)
+		if err != nil {
+			log.Printf("error looking up %q: %v", s, err)
+			continue
+		}
+		for _, a := range hostAddrs {
+			ua, err := net.ResolveUDPAddr("udp", net.JoinHostPort(a, port))
+			if err != nil {
+				log.Printf("error resolving %q: %v", a, err)
+				continue
+			}
+			addrs = append(addrs, dht.NewAddr(ua))
+		}
+	}
+	if len(addrs) == 0 {
+		err = errors.New("nothing resolved")
+	}
+	return
+}
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	tagflag.Parse(&flags)
+	clientConfig := torrent.Config{
+		Debug: false,
+		Seed:  true,
+	}
+	if flags.Addr != nil {
+		clientConfig.SetListenAddr(flags.Addr.String())
+	}
+	if flags.UploadRate != -1 {
+		clientConfig.UploadRateLimiter = rate.NewLimiter(rate.Limit(flags.UploadRate), 256<<10)
+	}
+	if flags.DownloadRate != -1 {
+		clientConfig.DownloadRateLimiter = rate.NewLimiter(rate.Limit(flags.DownloadRate), 1<<20)
+	}
+
+	clientConfig.DisableIPv6 = true
+	clientConfig.DhtStartingNodes = GlobalBootstrapAddrs
+	client, err := torrent.NewClient(&clientConfig)
+	if err != nil {
+		log.Fatalf("error creating client: %s", err)
+	}
+	defer client.Close()
+	// Write status on the root path on the default HTTP muxer. This will be
+	// bound to localhost somewhere if GOPPROF is set, thanks to the envpprof
+	// import.
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		client.WriteStatus(w)
+	})
+	if stdoutAndStderrAreSameFile() {
+		log.SetOutput(progress.Bypass())
+	}
+	progress.Start()
+	addTorrents(client)
+	if client.WaitAll() {
+		log.Print("downloaded ALL the torrents")
+	} else {
+		log.Fatal("y u no complete torrents?!")
+	}
+	if clientConfig.Seed {
+		select {}
+	}
+	expvar.Do(func(kv expvar.KeyValue) {
+		fmt.Printf("%s: %s\n", kv.Key, kv.Value)
+	})
+	envpprof.Stop()
+}
