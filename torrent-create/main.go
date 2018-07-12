4f8a70e89a0300a60ec19a84a8111c8ccf0041a4
liuwei <levonfly@gmail.com>
2018-07-12 15:35:49 +0800

    update

--- torrent-create/main.go
+++ torrent-create/main.go
@@ -66,7 +66,7 @@ func main() {
 
 var (
 	//builtinAnnounceList = "http://172.24.120.65:16185/announce,udp://172.24.120.65:16185"
-	builtinAnnounceList = "https://localhost.fhyx.online:6097/announce"
+	builtinAnnounceList = "https://haven.fhyx.online/announce"
 )
 
 func GetTrackAddrs() []string {
